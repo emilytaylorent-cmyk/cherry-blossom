de3d3171de263ca27abc46facc08acf4c1df04ad
PenguinDawn <157507535+PenguinDawn@users.noreply.github.com>
2026-04-20 15:47:24 -0500

    Update Designs.tsx

--- src/pages/Designs.tsx
+++ src/pages/Designs.tsx
@@ -1,10 +1,5 @@
 
 import '../index.css'
-import ProjectHolder from '../components/ProjectHolder'
-import Words from "../assets/Portfolio Website.png"
-import BBQSandwich from "../assets/bbq-sandwich-1.jpg"
-import BarMockup from "../assets/chocolatePics/Mockup-Chocolate-Bars.png"
-import { Link } from 'react-router-dom'
 
 
 

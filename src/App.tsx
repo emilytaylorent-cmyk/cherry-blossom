b13aba76496db9ac53b8692ba16f1d309b3bf6cd
PenguinDawn <157507535+PenguinDawn@users.noreply.github.com>
2026-04-20 15:19:31 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,35 @@
-// src/App.tsx (or main.tsx, depending on your setup)
-
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home.tsx';
+import Logo from './assets/pictures/Logo.svg'
 
-import Header from './components/Header';
-import Line1 from './assets/Line1.png'
 
-import Project from './pages/Project.tsx';
 
-import Websites from './pages/Websites.tsx';
-import Designs from './pages/Designs.tsx';
-import Footer from './components/Footer.tsx';
+import Projects from './pages/Project.tsx';
+import Contact from './pages/Logos.tsx';
+import About from './pages/Websites.tsx';
 
 function App() {
     return (
         <BrowserRouter>
-        <Link to="/" className="no-underline border-none shadow-none" style={{ textDecoration: 'inherit' }}><Header /></Link>
-            
-            <div className='bg-[#619394] gap-3 flex justify-center text-white h-full items-center'>
-                <Link to="/Websites" className=' hover:bg-[#D2E4E4] hover:text-black p-2 hover:cursor-pointer px-4'>
-                    <p>Websites</p>
-                </Link>
-                <img src={Line1} className='h-6' />
-                <Link to="/Designs" className=' hover:bg-[#D2E4E4] hover:text-black p-2 hover:cursor-pointer px-4'>
-                    <p className='no-underline'>Designs</p>
-                </Link>
-                                <img src={Line1} className='h-6' />
-                <Link to="/Project" className=' hover:bg-[#D2E4E4] hover:text-black p-2 hover:cursor-pointer px-4'>
-                    <p className='no-underline'>Project</p>
-                </Link>
+            <div className='w-full bg-[#f9f9f9] flex flex-row px-[5%] border-b-2 items-end border-[#C999BC]'>
+                <Link to="/"><img src={Logo} alt="Company Logo" width="200" /></Link>
+                {/* <p>Created with React, TypeScript, and Tailwind CSS</p> */}
 
+        
+                    <ul className='w-[40%] flex flex-row justify-between ml-auto'>
+                        <Link to="/About"><li className='text-[18px] font-["Kameron"] hover:bg-[#C999BC] hover:cursor-pointer p-4 w-30 text-center'>About</li> </Link>
+                        <Link to="/Projects"><li className='text-[18px] font-["Kameron"] hover:bg-[#C999BC] hover:cursor-pointer p-4 w-30 text-center'>Projects</li></Link>
+                        <Link to="/Contact"><li className='text-[18px] font-["Kameron"] hover:bg-[#C999BC] hover:cursor-pointer p-4 w-30 text-center'>Contact</li></Link>
+                    </ul>
             </div>
+            
+        
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/Project" element={<Project/>} />
-                <Route path="/Websites" element={<Websites/>} />
-                <Route path="/Designs" element={<Designs/>} />
-
+                <Route path="/About" element={<About />} />
+                <Route path="/Projects" element={<Projects/>} />
+                <Route path="/Contact" element={<Contact/>} />
             </Routes>
-             <Link to="/" className="!no-underline border-none shadow-none" style={{ textDecoration: 'inherit' }}><Footer /></Link>
         </BrowserRouter>
     );
 }
